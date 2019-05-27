e99b4ca5e514dd8d43b49c8de54b0b90fb5dec4b
Sunil L <sunil.l@cerner.com>
2019-05-27 18:51:59 +0530

    Person Toggle Working

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import './App.css';
+
+import Person from './Person/Person';
+
+class App extends Component{
+    //TODO : read the state from Mongodb for the id and get it as state object
+    state = {
+        persons:[
+            {id: "sl044390", name:'sunil', age: 28, alive: true},
+            {id: "hs000001", name:'hodor', age: 45, alive: false},
+            {id: "js999999", name:'jon snow', age: 34, alive:true}
+        ],
+        showPersons: false
+    }
+
+    switchNameHandler = () => {
+        this.setState(
+            {
+                persons:[
+                {id: "sl044390", name:'Sunil L', age: 28, alive: true},
+                {id: "hs000001", name:'Hodor Hodor', age: 60, alive: true},
+                {id: "js999999", name:'Aegon Targearyn', age: 80, alive:true}
+                ]
+            }
+        )
+    }
+
+    nameChangeHandler = (event) => {
+        console.log(event.target.value);
+    }
+
+    togglePersonHandler = () => {
+       const doesShow = this.state.showPersons;
+       this.setState({
+           showPersons : !doesShow
+       })
+    }
+
+    render(){
+        return(
+            <div className="App">
+                <h1>Person Data Handling Using React</h1>
+                <button onClick = {this.switchNameHandler}>Update Names </button>
+                <button onClick = {this.togglePersonHandler}>Toggle Names</button>
+                { this.state.showPersons === true ? 
+                <div>
+                    {/*filter only the alive characters and map them into the Person component*/}
+                    {this.state.persons.filter(person => person.alive).map(person => 
+                                                                                    <Person 
+                                                                                    key = {person.id} 
+                                                                                    name = {person.name} 
+                                                                                    age = {person.age} 
+                                                                                    changed = {this.nameChangeHandler}/>
+                                                                                )} 
+                </div> : null
+                } 
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Person.css';
+
+const Person = (props) => {
+    return(
+        <div className="Person">
+            <p>I am called the {props.name}</p>
+            <p>I am {props.age} old in Earth years</p>
+            <input type="text" onChange = {props.changed}/>
+        </div>
+    )
+}
+
+export default Person;
